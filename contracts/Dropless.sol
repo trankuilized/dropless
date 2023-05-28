bec480a3805e7fe02c2d0860438a399c5f3afa13
Trankuilized <mvinathan@gmail.com>
2023-05-28 19:50:52 -0400

    allow trades during pause w/ fee

--- contracts/Dropless.sol
+++ contracts/Dropless.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
@@ -14,13 +14,27 @@ contract Dropless is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+
+    struct PriceData {
+        uint256 price;
+        uint256 timestamp;
+    }
+
+    PriceData public priceData;
+
+    uint256 public timeLapse;
+    uint256 public priceDrop;
+    uint256 public pauseDuration;
+
+    uint256 private lastPauseEndTime;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() initializer public {
+    function initialize(uint256 initialSupply) initializer public {
         __ERC20_init("Dropless", "DROP");
         __ERC20Burnable_init();
         __Pausable_init();
@@ -28,12 +42,94 @@ contract Dropless is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
         __ERC20Permit_init("Dropless");
         __UUPSUpgradeable_init();
 
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _mint(msg.sender, 50000 * 10 ** decimals());
-        _grantRole(MINTER_ROLE, msg.sender);
-        _grantRole(UPGRADER_ROLE, msg.sender);
+        _mint(msg.sender, initialSupply);
+        timeLapse = 1 hours;
+        priceDrop = 10;
+        pauseDuration = 12 hours;
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(UPGRADER_ROLE, msg.sender);
+        _setupRole(OWNER_ROLE, msg.sender);
     }
+
+uint256[] public pauseDurations;  // Durations for each price decrease range
+uint256[] public priceDecreaseRanges;  // Price decrease ranges (e.g., [10, 20, 30] for ranges of up to 10%, 20%, 30%)
+
+function updatePriceData(uint256 _price, uint256 _timestamp) external {
+    uint256 priceDecrease = (priceData.price - _price) * 100 / priceData.price;
+    if (priceData.price > 0 && priceDecrease > priceDrop) {
+        if (_timestamp - priceData.timestamp <= timeLapse && block.timestamp >= lastPauseEndTime) {
+            _pause();
+
+            // Determine pause duration based on price drop
+            for (uint256 i = 0; i < priceDecreaseRanges.length; i++) {
+                if (priceDecrease <= priceDecreaseRanges[i]) {
+                    pauseDuration = pauseDurations[i];
+                    break;
+                }
+            }
+
+            lastPauseEndTime = block.timestamp + pauseDuration;
+        }
+    }
+    priceData.price = _price;
+    priceData.timestamp = _timestamp;
+}
+
+// Allow the owner to update the pause durations and price decrease ranges
+function setPauseDurations(uint256[] calldata newPauseDurations) external onlyRole(OWNER_ROLE) {
+    pauseDurations = newPauseDurations;
+}
+
+function setPriceDecreaseRanges(uint256[] calldata newPriceDecreaseRanges) external onlyRole(OWNER_ROLE) {
+    priceDecreaseRanges = newPriceDecreaseRanges;
+}
+
+    function forceUnpause() external onlyRole(OWNER_ROLE) {
+        _unpause();
+        lastPauseEndTime = block.timestamp;
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+address public feeReceiver;  // The address that receives the fees
+uint256 public feePercentage;  // The fee percentage (0 - 100)
+
+function setFeeReceiver(address _feeReceiver) external onlyRole(OWNER_ROLE) {
+    feeReceiver = _feeReceiver;
+}
+
+function setFeePercentage(uint256 _feePercentage) external onlyRole(OWNER_ROLE) {
+    require(_feePercentage <= 100, "Dropless: feePercentage cannot be more than 100");
+    feePercentage = _feePercentage;0
+}
+
+function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+    if (paused()) {
+        require(to != address(this), "Dropless: Cannot send to the contract while paused");
+
+        uint256 fee = (amount * feePercentage) / 100; // calculates fee as a percentage of the transfer amount
+        uint256 amountAfterFee = amount - fee;
+
+        require(amountAfterFee + fee <= balanceOf(from), "Dropless: Transfer amount + fee exceeds balance");
+
+        _transfer(from, feeReceiver, fee); // transfer the fee
+        super._beforeTokenTransfer(from, to, amountAfterFee); // transfer the remaining amount
+    } else {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
+
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
 
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
@@ -42,22 +138,4 @@ contract Dropless is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, 
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, amount);
-    }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        onlyRole(UPGRADER_ROLE)
-        override
-    {}
 }
